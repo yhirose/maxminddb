102144c13fe1cdb8fe44bd244d2c70564692b601
johannes.barre@wimdu.com <johannes.barre@wimdu.com>
2014-04-01 12:32:35 +0200

    Specs rewritten

--- spec/maxminddb_spec.rb
+++ spec/maxminddb_spec.rb
@@ -1,18 +1,23 @@
 require 'maxminddb'
 
 describe MaxMindDB do
-  IP = '74.125.225.224'
+  let(:city_db) { MaxMindDB.new('spec/GeoLite2-City.mmdb') }
+  let(:country_db) { MaxMindDB.new('spec/GeoLite2-Country.mmdb') }
 
-  before do
-    @city = MaxMindDB.new('spec/GeoLite2-City.mmdb')
-    @country = MaxMindDB.new('spec/GeoLite2-Country.mmdb')
-  end
+  context 'for the ip 74.125.225.224' do
+    let(:ip) { '74.125.225.224' }
 
-  it 'should ' do
-    @city.lookup(IP)['city']['names']['en'].should == 'Mountain View'
-    @city.lookup(IP)['location']['longitude'].should == -122.0574
+    it 'returns Mountain View as the English name' do
+      expect(city_db.lookup(ip)['city']['names']['en']).to eq('Mountain View')
+    end
 
-    @country.lookup(IP)['country']['names']['en'].should == 'United States'
+    it 'returns -122.0574 as the longitude' do
+      expect(city_db.lookup(ip)['location']['longitude']).to eq(-122.0574)
+    end
+
+    it 'returns United States as the English country name' do
+      expect(country_db.lookup(ip)['country']['names']['en']).to eq('United States')
+    end
   end
 end
 
