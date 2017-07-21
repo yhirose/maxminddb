cfbacdbc41b8dd8518e4ea7159c9705b4e581c68
Marvin Baltazar <marvin.baltazar@gmail.com>
2017-07-21 13:49:32 +0800

    Expose accuracy_radius in Location data

--- spec/maxminddb/result/location_spec.rb
+++ spec/maxminddb/result/location_spec.rb
@@ -9,13 +9,15 @@ describe MaxMindDB::Result::Location do
       "latitude"=>37.419200000000004,
       "longitude"=>-122.0574,
       "metro_code"=>"807",
-      "time_zone"=>"America/Los_Angeles"
+      "time_zone"=>"America/Los_Angeles",
+      "accuracy_radius"=>1000
     } }
 
     its(:latitude) { should eq(37.419200000000004) }
     its(:longitude) { should eq(-122.0574) }
     its(:metro_code) { should eq("807") }
     its(:time_zone) { should eq("America/Los_Angeles") }
+    its(:accuracy_radius) { should eq(1000) }
   end
 
   context "without a result" do
@@ -25,5 +27,6 @@ describe MaxMindDB::Result::Location do
     its(:longitude) { should be_nil }
     its(:metro_code) { should be_nil }
     its(:time_zone) { should be_nil }
+    its(:accuracy_radius) { should be_nil }
   end
 end
